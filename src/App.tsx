eea7038c616e266fc692c729dc5f02ec73e05411
kidus hintsa <kidus908@gmail.com>
2024-08-28 22:44:43 +0300

    making responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
-import './App.css'
+// import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className=''>Game discovery</h1>
-      <button className='btn btn-primary'>add</button>
-    </>
+  
+     <section className='container-fluid border'>
+      <div className="row">
+        <div className="col-12 bg-primary">nav</div>
+        <div className="col-6 d-none d-lg-block bg-danger">aside</div>
+        <div className="col bg-secondary">main</div>
+      </div>
+     </section>
+   
   )
 }
 
